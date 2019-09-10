ba863a8501fbbb0834ad025fc35b7268f7811774
Dennis S.Otavio <39060358+denisotavio1992@users.noreply.github.com>
2019-09-10 11:59:29 -0300

    Aula 20 mensagem de erro de validação
    Mensagem para email einvalido e senha que nao atende os requisitosa minimos !!!

--- login/src/app/auth/pages/login/login.page.ts
+++ login/src/app/auth/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +20,17 @@ export class LoginPage implements OnInit {
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
+  get name(): FormControl {
+    return this.authForm.get('name') as FormControl;
+  }
 
+  get email(): FormControl {
+    return this.authForm.get('email') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.authForm.get('password') as FormControl;
+  }
   onSubmit(): void {
     console.log('authform: ', this.authForm.value);
   }
