d054176617cc3fb77c904409bb422ec8bd06b8ff
Dennis S.Otavio <39060358+denisotavio1992@users.noreply.github.com>
2019-10-28 22:17:17 -0300

    Aula 79

--- login/src/app/tasks/pages/task-save/task-save.page.ts
+++ login/src/app/tasks/pages/task-save/task-save.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { TasksService } from '../../services/tasks.service';
+import { NavController } from '@ionic/angular';
+import { OverlayService } from 'src/app/core/services/overlay.service';
+
 @Component({
   selector: 'app-task-save',
   templateUrl: './task-save.page.html',
@@ -9,7 +13,12 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class TaskSavePage implements OnInit {
   taskForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private navCtrl: NavController,
+    private overlayService: OverlayService,
+    private taskService: TasksService
+  ) {}
 
   ngOnInit() {
     this.createForm();
@@ -21,7 +30,21 @@ export class TaskSavePage implements OnInit {
     });
   }
 
-  onSubmit(): void {
-    console.log('task: ', this.taskForm.value);
+  async onSubmit(): Promise<void> {
+    const loading = await this.overlayService.loading({
+      message: 'Salvando...'
+    });
+    try {
+      const task = await this.taskService.create(this.taskForm.value);
+      console.log('Task criada', task);
+      this.navCtrl.navigateBack('/tasks');
+    } catch (error) {
+      console.log('Erro ao salvar a lista: ', error);
+      await this.overlayService.toast({
+        message: error.message
+      });
+    } finally {
+      loading.dismiss();
+    }
   }
 }
