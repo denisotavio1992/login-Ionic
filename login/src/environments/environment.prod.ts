33654b28a9db7f3ba60973f3e46a68a0e7445ae5
Dennis S.Otavio <39060358+denisotavio1992@users.noreply.github.com>
2019-11-05 15:57:50 -0300

    Fim da sessao

--- login/src/environments/environment.prod.ts
+++ login/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDdrvDr_XcvNBLj4yA1FxFvKgzK3qztm1Y',
+    authDomain: 'login-generico.firebaseapp.com',
+    databaseURL: 'https://login-generico.firebaseio.com',
+    projectId: 'login-generico',
+    storageBucket: '',
+    messagingSenderId: '533668085751',
+    appId: '1:533668085751:web:4c62db2e8989aa453db8f2'
+  }
 };
